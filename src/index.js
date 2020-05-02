43abc8f216cc0bd4aeb43cdb24962c6d7e73ea88
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-02 19:00:27 +0530

    Created store and updated API

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import "bulma";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,27 +1,22 @@
 import {
-    _getUsers,
-    _getUser,
-    _getQuestions,
-    _saveQuestion,
-    _saveQuestionAnswer
+  _getUsers,
+  _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
 } from "./_DATA";
 
-export function saveQuestion (info) {
-    return _saveQuestion(info);
+export function saveQuestion(info) {
+  return _saveQuestion(info);
 }
 
-export function saveQuestionAnswer (info) {
-    return _saveQuestionAnswer(info);
-}
-
-export function getUser (id) {
-    return _getUser(id);
+export function saveQuestionAnswer(info) {
+  return _saveQuestionAnswer(info);
 }
 
 export function getUsers() {
-    return _getUsers();
+  return _getUsers();
 }
 
 export function getQuestions() {
-    return _getQuestions();
+  return _getQuestions();
 }
