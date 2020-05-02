0422eaf02ec89523115eb6bb22b9d985c750ba5d
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-02 23:57:07 +0530

    Setting authed user working correctly

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,17 +1,12 @@
 import { getUsers } from "../utils/API";
 import { setUsers } from "./users";
-import { setAuthedUser } from "./authedUser";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
-
-const AUTHED_USER_ID = "sarahedo";
 
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
-    return getUsers().then(({ users }) => {
-      //   console.log(users);
+    return getUsers().then((users) => {
       dispatch(setUsers(users));
-      dispatch(setAuthedUser(AUTHED_USER_ID));
       dispatch(hideLoading());
     });
   };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
 
-export default class Login extends Component {
+class Login extends Component {
+  state = {
+    selectedUser: "",
+  };
+
+  handleDropdown = (e) => {
+    const selectedUser = e.target.value;
+    this.setState(() => ({
+      selectedUser,
+    }));
+  };
+
+  handleLogin = () => {
+    if (!this.state.selectedUser) {
+      alert("Please select the user!");
+    } else {
+      this.props.dispatch(setAuthedUser(this.state.selectedUser));
+    }
+  };
+
   render() {
     return (
       <div className="section has-background-light">
@@ -8,15 +29,22 @@ export default class Login extends Component {
         <div className="field has-addons">
           <div className="control is-expanded">
             <div className="select is-fullwidth">
-              <select name="user">
-                <option value="Argentina">Argentina</option>
-                <option value="Bolivia">Bolivia</option>
-                <option value="Brazil">Brazil</option>
+              <select onChange={this.handleDropdown}>
+                <option>Choose user</option>
+                {Object.keys(this.props.users).map((userId, index) => (
+                  <option key={index} value={this.props.users[userId].id}>
+                    {this.props.users[userId].name}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
           <div className="control">
-            <button type="submit" className="button is-primary">
+            <button
+              type="submit"
+              className="button is-primary"
+              onClick={this.handleLogin}
+            >
               Login
             </button>
           </div>
@@ -25,3 +53,11 @@ export default class Login extends Component {
     );
   }
 }
+
+function mapStateToProps({ users }) {
+  return {
+    users,
+  };
+}
+
+export default connect(mapStateToProps)(Login);
