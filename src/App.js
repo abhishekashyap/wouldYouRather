51ae7232a8afd2562086c7533635bb9c28ecbf01
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-02 19:11:09 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/shared/Navbar";
 import Header from "./components/shared/Header";
-import ToggleQuestionBar from "./components/shared/ToggleQuestionBar";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Add from "./pages/Add";
 import Leaderboard from "./pages/Leaderboard";
-import AnswerQuestion from "./components/AnswerQuestion/AnswerQuestion";
 
 function App() {
   return (
-    <div className="container">
+    <Router>
+      {/* Show navbar if the user is authed */}
       <Navbar />
       <Header />
-      <ToggleQuestionBar />
-      <Login />
-      <Home />
-      <AnswerQuestion />
-      <Add />
-      <Leaderboard />
-    </div>
+      <Route path="/" component={Home} exact />
+      <Route path="/login" component={Login} exact />
+      <Route path="/leaderboard" component={Leaderboard} exact />
+      <Route path="/add" component={Add} exact />
+      <Route path="/question/:id" component={Add} />
+    </Router>
   );
 }
 
