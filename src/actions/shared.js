894b842c287f8fffee9867732ef4be79032cd88f
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-02 17:10:19 +0530

    Created pseudo API

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,4 @@
-import { _getUsers } from "../utils/_DATA";
+import { getUsers } from "../utils/API";
 import { setUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
@@ -8,8 +8,8 @@ const AUTHED_USER_ID = "sarahedo";
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
-    return _getUsers().then(({ users }) => {
-    //   console.log(users);
+    return getUsers().then(({ users }) => {
+      //   console.log(users);
       dispatch(setUsers(users));
       dispatch(setAuthedUser(AUTHED_USER_ID));
       dispatch(hideLoading());
---
+++ src/utils/API.js
@@ -0,0 +1,27 @@
+import {
+    _getUsers,
+    _getUser,
+    _getQuestions,
+    _saveQuestion,
+    _saveQuestionAnswer
+} from "./_DATA";
+
+export function saveQuestion (info) {
+    return _saveQuestion(info);
+}
+
+export function saveQuestionAnswer (info) {
+    return _saveQuestionAnswer(info);
+}
+
+export function getUser (id) {
+    return _getUser(id);
+}
+
+export function getUsers() {
+    return _getUsers();
+}
+
+export function getQuestions() {
+    return _getQuestions();
+}
