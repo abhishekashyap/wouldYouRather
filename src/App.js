816d14867fe9b638d21be9a9ca607ba061af2514
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-02 12:17:30 +0530

    Created basic login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <div className="container">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <>
+        <p class="title is-2 is-center">Login</p>
+        <div class="field has-addons">
+          <div class="control is-expanded">
+            <div class="select is-fullwidth">
+              <select name="user">
+                <option value="Argentina">Argentina</option>
+                <option value="Bolivia">Bolivia</option>
+                <option value="Brazil">Brazil</option>
+              </select>
+            </div>
+          </div>
+          <div class="control">
+            <button type="submit" class="button is-primary">
+              Login
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
