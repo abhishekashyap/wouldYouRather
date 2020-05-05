5ee2a36b32063ae100f954c2816b1aa433531824
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-05 18:02:38 +0530

    Adding question to user

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,26 @@
 export const SET_USERS = "SET_USERS";
+export const ADD_USER_QUESTION_ANSWER = "ADD_USER_QUESTION_ANSWER";
+export const ADD_USER_QUESTION = "ADD_USER_QUESTION";
 
 export function setUsers(users) {
   return {
     type: SET_USERS,
     users,
+  };
+}
+
+export function addUserQuestion(question) {
+  return {
+    type: ADD_USER_QUESTION,
+    question,
+  };
+}
+
+export function addUserQuestionAnswer(authedUser, questionId, selectedOption) {
+  return {
+    type: ADD_USER_QUESTION_ANSWER,
+    authedUser,
+    questionId,
+    selectedOption,
   };
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,8 @@
-import { SET_USERS } from "../actions/users";
+import {
+  SET_USERS,
+  ADD_USER_QUESTION,
+  ADD_USER_QUESTION_ANSWER,
+} from "../actions/users";
 
 export default function setUsers(state = {}, action) {
   switch (action.type) {
@@ -6,6 +10,29 @@ export default function setUsers(state = {}, action) {
       return {
         ...state,
         ...action.users,
+      };
+
+    case ADD_USER_QUESTION:
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: [
+            ...state[action.question.author].questions,
+            action.question.id,
+          ],
+        },
+      };
+    case ADD_USER_QUESTION_ANSWER:
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...state[action.authedUser],
+          answers: {
+            ...state[action.authedUser].answers,
+            [action.questionId]: action.selectedOption,
+          },
+        },
       };
 
     default:
